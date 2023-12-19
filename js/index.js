3322b54220f5d4b4271ad52d69025e4cb1bdd4f2
nadir0glu <nadiroglunail@gmail.com>
2023-12-18 22:22:42 -0500

    smth missing

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
-document.addEventListener("DOMContentLoaded", function() {});
+// document.addEventListener("DOMContentLoaded", function() {});
+
+fetch("http://localhost:3000/books")
+.then(res => res.json())
+.then(data => {
+  console.log(data)
+  for (books of data)
+  displayTitles(books)
+})
+
+const displayTitles = (bookObjects) => {
+  let li = document.createElement('li')
+  li.textContent = bookObjects.title
+  let ul = document.getElementById("list")
+  ul.append(li)
+  li.addEventListener('click', () => {
+        
+    showTitles(bookObjects);
+
+})
+    
+}
+const showTitles = (restObjects) => {
+    let thumbNail = document.createElement('img')
+    thumbNail.src = restObjects.img_url
+
+    let description = document.createElement('p')
+    description.textContent = restObjects.description
+
+    let listUsers = document.createElement('ul')
+    
+    
+    let users = document.createElement('li')
+    users.textContent = restObjects.users
+    listUsers.append(users)
+
+    let div = document.getElementById("show-panel")
+    div.append(thumbNail, description, listUsers)
+
+}
+
+
+
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,12 +1,24 @@
+fetch("http://localhost:3000/books")
+.then(res => res.json())
+.then(data => {
+  console.log(data)
+})
+
+const displayTitles = (bookObjects) => {
+  let li = document.createElement('li')
+  li.textContent = bookObjects.
+
+
+}
 // The code below ensures that students who are using CodeGrade will get credit
 // for the code-along in Canvas; you can disregard it.
 
-require("./helpers.js");
+// require("./helpers.js");
 
-describe("", () => {
-  describe("", () => {
-    it("Test passing", () => {
-      return true;
-    });
-  });
-});
+// describe("", () => {
+//   describe("", () => {
+//     it("Test passing", () => {
+//       return true;
+//     });
+//   });
+// });
